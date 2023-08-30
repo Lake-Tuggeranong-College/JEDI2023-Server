79a1817bf014d4fae0a1c5cd574ede4b26ff40c7
Trixelit <36838998+Trixelit@users.noreply.github.com>
2023-08-30 15:44:14 +1000

    Add uploadData code

--- uploadData.php
+++ uploadData.php
@@ -1,7 +1,7 @@
 <?php
     include "config.php";
 
-    $api_key = $userName = $moduleName = $moduleData = "";
+    $userName = $moduleName = $moduleData = "";
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         // Takes raw data from the request
@@ -14,9 +14,21 @@
         $userName = $data->userName;
         $moduleData = $data->moduleData;
         $moduleName = $data->moduleName;
-        $api_key = $data->api_key;
+        //$api_key = $data->api_key;
         date_default_timezone_set('Australia/Canberra');
         $date = date("Y-m-d H:i:s");
+
+        $sql = "INSERT INTO moduleData (moduleName, moduleData, userName, datePosted) VALUES (:moduleName, :moduleData, :userName,:datePosted)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':userName', $userName);
+        $stmt->bindValue(':moduleData', $moduleData);
+        $stmt->bindValue(':moduleName', $moduleName);
+        $stmt->bindValue(':datePosted', $date);
+        $stmt->execute();   
+        $conn->close();
+
+        echo("Data submitted");
+
     } else {
         echo("This page is only accessible via POSTing from ESP32s...");
     }
