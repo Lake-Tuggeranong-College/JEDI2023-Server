fe38658a3fc4c908dda3809a9b739330f8870295
Trixelit <36838998+Trixelit@users.noreply.github.com>
2023-08-31 12:25:44 +1000

    Code separation completed, tested and working

--- uploadData.php
+++ uploadData.php
@@ -1,7 +1,7 @@
 <?php
     include "config.php";
 
-    $userName = $moduleName = $moduleData = "";
+    $api_key = $userName = $moduleName = $moduleData = "";
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         // Takes raw data from the request
@@ -18,17 +18,14 @@
         date_default_timezone_set('Australia/Canberra');
         $date = date("Y-m-d H:i:s");
 
+        //DO NOT CHANGE THIS DATE CODE, MUST STAY SAME TO WORK WITH MYSQL
         $sql = "INSERT INTO moduleData (moduleName, moduleData, userName, datePosted) VALUES (:moduleName, :moduleData, :userName,:datePosted)";
         $stmt = $conn->prepare($sql);
         $stmt->bindValue(':userName', $userName);
         $stmt->bindValue(':moduleData', $moduleData);
         $stmt->bindValue(':moduleName', $moduleName);
         $stmt->bindValue(':datePosted', $date);
-        $stmt->execute();   
-        $conn->close();
-
-        echo("Data submitted");
-
+        $stmt->execute();
     } else {
         echo("This page is only accessible via POSTing from ESP32s...");
     }
